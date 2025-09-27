0d833b0b452f35b01848d467586fe3d827351856
DnvMaster <dnvcomp@yandex.ru>
2025-09-27 15:25:47 +0300

    Counter Increment & Decrement

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <h1>{{ $count }}</h1>
+    <button wire:click="increment">Увеличить</button>
+    <button wire:click="decrement">Уменьшить</button>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\HelloMessage;
+use App\Livewire\Counter;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('hello', HelloMessage::class)->name('hello.message');
+Route::get('counter', Counter::class)->name('counter');
