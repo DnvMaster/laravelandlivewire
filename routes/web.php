9db838377dae8126abeb7cd2ac6fb3eaae1e5e82
DnvMaster <dnvcomp@yandex.ru>
2025-09-27 15:03:57 +0300

    Hello DnvMaster

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\HelloMessage;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello', HelloMessage::class)->name('hello.message');
